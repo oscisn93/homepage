b786f959f8b992e11ade45d78944fe7b310b2846
Oscar <41174627+oscisn93@users.noreply.github.com>
2022-07-03 01:00:00 -0700

    Added server-side form validation and implemented declarative indentation for dynamically rendered html

--- db.inc.php
+++ db.inc.php
@@ -1,9 +1,15 @@
 <?php
+    // error_reporting(E_ERROR | E_PARSE);
 
-    $servername = "mariadb";
-    $username = "cs332m3";
-    $password = "YSF4EsHY";
-    $database = "cs332m3";
+    // $servername = "mariadb";
+    // $username = "cs332m3";
+    // $password = "YSF4EsHY";
+    // $database = "cs332m3";
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "university";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
@@ -13,52 +19,160 @@
     }
 
     function getcourses($student_cwid, $conn){
-        echo "<br />";
-        $sql_query="SELECT title, grade FROM enrollment, course WHERE E_CWID =".$student_cwid." AND enrollment.E_cno = course.cno;";
-        $result = $conn->query($sql_query);
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()) {
-                echo "title: ".$row["title"]." Grade: ".$row["grade"]."<br />";
+        if(is_numeric($student_cwid) && strlen($student_cwid) == 9){
+            $sql_query="SELECT title, grade 
+                        FROM enrollment, course 
+                        WHERE E_CWID =".$student_cwid." AND 
+                        enrollment.E_cno = course.cno;";
+            $result = $conn->query($sql_query);
+            if($result->num_rows > 0){
+                echo '<br /><br />'.
+                    '<table class="table table-dark">'.
+                        '<thead>'.
+                            '<th scope="col">Course Title</th>'.
+                            '<th scope="col">Grade</th>'.
+                        '</thead>';
+                while($row = $result->fetch_assoc()) {
+                echo '<tbody>'.
+                        '<th scope="row">'.
+                            $row["title"].
+                        "</th>".
+                        "<td>".
+                            $row["grade"].
+                        '</td>'.
+                    '</tbody>';
+                }
+                echo "</table>";
+            } else {
+                echo "No records found";
             }
         } else {
-            echo "No records found";
+            echo "Enter valid CWID";
         }
     }
+
     function getAllcourses($cno, $conn){
-        echo "<br />";
-        $sql_query="SELECT sno, classroom, meeting_days, start_time, end_time,E_cwid FROM section, enrollment WHERE section.scno = ".$cno." AND section.scno= enrollment.E_cno AND section.sno = enrollment.E_sno";
-        $result = $conn->query($sql_query);
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()) {
-                echo "section Number: ".$row["sno"]." Class: ".$row["classroom"]." Meeting Days: ".$row["meeting_days"]." Time: ".$row["start_time"]." - ".$row["end_time"]." Student CWID: ".$row["E_cwid"]."<br />";
+        if(is_numeric($cno) && strlen($cno) == 4){
+            $sql_query="SELECT sno, classroom, 
+                        meeting_days, start_time, 
+                        end_time, E_cwid 
+                        FROM section, enrollment 
+                        WHERE section.scno = ".$cno." AND 
+                        section.scno= enrollment.E_cno AND 
+                        section.sno = enrollment.E_sno";
+            $result = $conn->query($sql_query);
+            if($result->num_rows > 0){
+                echo '<br /><br />'.
+                    '<table class="table table-dark">'.
+                        '<thead>'.
+                            '<th scope="col">Section Number</th>'.
+                            '<th scope="col">Classroom</th>'.
+                            '<th scope="col">Meeting Days</th>'.
+                            '<th scope="col">Time</th>'.
+                            '<th scope="col">Student CWID</th>'.
+                        '</thead>';
+                while($row = $result->fetch_assoc()) {
+                    echo "<tbody>".
+                            "<th scope='row'>".
+                                $row["sno"].
+                            "</th>".
+                            "<td>".
+                                $row["classroom"].
+                            "</td>".
+                            "<td>".
+                                $row["meeting_days"].
+                            "</td>".
+                            "<td>".
+                                trim($row["start_time"])." - ".$row["end_time"].
+                            "</td>".
+                            "<td>".
+                                $row["E_cwid"].
+                            "</td>".
+                        "</tbody>";
+                }
+                echo "</table>";
+            } else {
+                echo "No such sections found";
             }
         } else {
-            echo "No records found";
+            echo "Enter a valid course number!";
         }
     }
+
     function getProfInfo($ssn, $conn){
-        echo "<br />";
-        $sql_query="SELECT course.title, classroom, meeting_days, start_time, end_time FROM professor, section, course WHERE professor.ssn = ".$ssn." AND professor.ssn = section.instructor_ssn AND section.scno = course.cno";
-        $result = $conn->query($sql_query);
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()) {
-                echo "title: ".$row["title"]." Class: ".$row["classroom"]." Meeting Days: ".$row["meeting_days"]." Time: ".$row["start_time"]." - ".$row["end_time"]."<br />";
+        if(is_numeric($ssn) && strlen($ssn) == 9){
+            $sql_query="SELECT course.title, classroom, 
+                        meeting_days, start_time, end_time 
+                        FROM professor, section, course 
+                        WHERE professor.ssn = ".$ssn." AND 
+                        professor.ssn = section.instructor_ssn AND 
+                        section.scno = course.cno";
+            $result = $conn->query($sql_query);
+            if($result->num_rows > 0){
+                echo '<br /><br />'.
+                '<table class="table table-dark">'.
+                    '<thead>'.
+                        '<th scope="col">Course Title</th>'.
+                        '<th scope="col">Classroom</th>'.
+                        '<th scope="col">Meeting Days</th>'.
+                        '<th scope="col">Time</th>'.
+                    '</thead>';
+                while($row = $result->fetch_assoc()) {
+                    echo "<tbody>".
+                            "<th scope='row'>".
+                                $row["title"].
+                            "</th>".
+                            "<td>".
+                                $row["classroom"].
+                            "</td>".
+                            "<td>".
+                                $row["meeting_days"].
+                            "</td>".
+                            "<td>".
+                                $row["start_time"]." - ".$row["end_time"].
+                            "</td>".
+                        "</tbody>";
+                }
+                echo "</table>";
+            } else {
+                echo "No records found";
             }
         } else {
-            echo "No records found";
+            echo "Enter a valid SSN";
         }
     }
+    
     function getcourseGrades($cno, $sno, $conn){
-        echo "<br />";
-        $sql_query="SELECT grade, COUNT(*) FROM enrollment WHERE E_cno = ".$cno." AND E_sno = ".$sno." GROUP BY grade";
-        $result = $conn->query($sql_query);
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()) {
-                echo "Grader: ".$row["grade"]." Count: ".$row["COUNT(*)"]."<br />";
+        $num = $cno.$sno;
+        if(is_numeric($num) && strlen($num) == 5){
+            $sql_query="SELECT grade, COUNT(*) 
+                        FROM enrollment 
+                        WHERE E_cno = ".$cno." AND 
+                        E_sno = ".$sno." GROUP BY grade";
+            $result = $conn->query($sql_query);
+            if($result->num_rows > 0){
+                echo '<br /><br />'.
+                '<table class="table table-dark">'.
+                    '<thead>'.
+                        '<th scope="col">Grade</th>'.
+                        '<th scope="col">Number Received</th>'.
+                    '</thead>';
+                while($row = $result->fetch_assoc()) {
+                    echo "<tbody>".
+                            "<th scope='row'>".
+                                $row["grade"].
+                            "</th>".
+                            "<td>".
+                                $row["COUNT(*)"].
+                            "</td>".
+                        "</tbody>";
+                }
+                echo "</table>";
+            } else {
+                echo "No records found";
             }
         } else {
-            echo "No records found";
+            echo "Enter valid course and section numbers!";
         }
     }
-    $conn->close();
 ?>
